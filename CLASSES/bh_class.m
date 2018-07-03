ea847b6369b3cbfa6bdf1b780e9f6b4f3309a9db
mcurti <31895988+mcurti@users.noreply.github.com>
2018-07-03 10:33:53 +0200

    bh_class modified
    - The BH curve is post processed, so that the inflection points are removed.

--- CLASSES/bh_class.m
+++ CLASSES/bh_class.m
@@ -14,9 +14,9 @@ classdef bh_class
             if      material_ID==1
 %                 [H,B] = textscan('BH_Cogent_M800_50A_50Hz.txt','%f');
 %                 Bsat=(B(end-1)+B(end))./2; % Saturation field, no tangent further
-                
+%                 
 %                 Bmin=0.1379;
-                %     Interpolation
+%                     Interpolation
 %                 mu0 = 4*pi*1e-7;
 %                 SB  = spline(B,H);
 %                 x0  = Bmod;
@@ -27,12 +27,30 @@ classdef bh_class
                 out = textscan(fileID,'%f %f');  H = out{1}; B = out{2};
                 fclose(fileID);
                 mu0 = 4*pi*1e-7;
-                ik = B>0 & B<0.9;
-                B(ik) = []; H(ik) = [];
+                mu_init = (B(1)-B(2))/((H(1)-H(2))*mu0);
+                mu_end  = (B(end-1)-B(end))/((H(end-1)-H(end))*mu0);
+%                 ik = B>0 & B<0.9;
+%                 B(ik) = []; H(ik) = [];
+                htest = H; btest = B;
+                bad_points = true(numel(btest));
                 
-                SB   = spline(B,[1/(1.6e4*mu0); H; 1/mu0]);%
+                while sum(bad_points)>0
+                    SBtest     = spline(btest,[1/(mu_init*mu0); htest; 1/(mu_end*mu0)]);
+                    SBextest   = fnxtr(SBtest,2);
+                    fpptest    = fnder(SBextest,2);
+                    bad_points = ppval(fpptest,btest)<0; 
+                    bad_points(1) = false;
+                    btest(bad_points) = []; htest(bad_points) = [];
+                mu_init = (btest(1)-btest(2))/((htest(1)-htest(2))*mu0);
+                mu_end  = (btest(end-1)-btest(end))/((htest(end-1)-htest(end))*mu0);
+                end
+                
+                
+                
+                SB   = spline(btest,[1/(mu_init*mu0); htest; 1/(mu_end*mu0)]);
                 SBex = fnxtr(SB,2);
                 fp  = fnder(SBex,1);
+                
             else
                 warning ('Undefined material option - Please import new material data as explained in description');
             end
