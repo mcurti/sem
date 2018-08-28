c6fe66d1d93a2a49b0fd3efdd1f805c924e77864
kbastiaens <k.bastiaens@tue.nl>
2018-08-28 18:20:38 +0200

    Added skin depth layer

--- DEMOS/SEM_v3/Transformer/transformer_SEM.m
+++ DEMOS/SEM_v3/Transformer/transformer_SEM.m
@@ -20,31 +20,53 @@ phi_p = S.PProcessing.flux_linkage(27);
 phi_s = S.PProcessing.flux_linkage(15);
 
 % Current density in plate
-N_el = 42;
-f = 1;
-sigma = 8.41e3;
+N_el = 49;                                                                          % Number of elements
+sigma_r = [6,12,18,24,30,36,42,43,44,45,46,47,48,49];                               % Elements having non-zero conductivity
+f = 1;                                                                              % Frequency [Hz]
+sigma_1 = 0;                                                                        % zero conductivity
+sigma_2 = 8.41e3;                                                                   % Non-zero conductivity [S/mm]
 J_e = cell(1,N_el);
 for k = 1:N_el
-    J_e{k} = real(S.PProcessing.Potential{k}*1j*2*pi*f*sigma);
+    if any(k == sigma_r)
+        J_e{k} = real(S.PProcessing.Potential{k}*1j*2*pi*f*sigma_2);
+    else
+        J_e{k} = real(S.PProcessing.Potential{k}*1j*2*pi*f*sigma_1);
+    end
 end
 
 %% Plot results
 
 % Plot the vector potential
 figure(1)
-clf
 hold on;
 % Plot points of geometry
 S.Geometry.plot_geometry('markersize',12,'color','k','points');
 % Plot lines of geometry
 S.Geometry.plot_geometry('color','k','lines');
 % Plot the vector potential
-% S.PProcessing.plot_contour(linspace(0,max(real(S.PProcessing.PHI))));
-S.PProcessing.plot_surf_var(J_e);
-hold off
+S.PProcessing.plot_contour(linspace(0,max(real(S.PProcessing.PHI))));
+hold off;
 c = colorbar;
-% caxis([min(real(S.PProcessing.PHI))*0 max(real(S.PProcessing.PHI))])
+caxis([min(real(S.PProcessing.PHI))*0 max(real(S.PProcessing.PHI))])
 title(c,'A_z')
 xlabel('x [mm]')
 ylabel('y [mm]')
-daspect([1,1,1e4])
+axis equal
+
+% Plot the current density
+figure(2);
+hold on;
+% Plot points of geometry
+S.Geometry.plot_geometry('markersize',12,'color','k','points');
+% Plot lines of geometry
+S.Geometry.plot_geometry('color','k','lines');
+% Plot the current density in the geometry
+S.PProcessing.plot_surf_var(J_e);
+hold off;
+c = colorbar;
+title(c,'J_z')
+xlabel('x [mm]')
+ylabel('y [mm]')
+axis equal
+
+
